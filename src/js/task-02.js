f144da4aebb0e880b0224242beb2d49bbd3452b6
Yurii <ivaschenko_u@ukr.net>
2021-11-09 17:36:30 +0200

    1,2

---
+++ src/js/task-02.js
@@ -0,0 +1,20 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+// const ulRef = document.querySelector('#ingredients');
+
+// const markap = ingredients.map(element => {
+//   const liRef = document.createElement("li");
+//   liRef.classList.add('item');
+//   liRef.textContent = element;
+//   return liRef;
+// });
+// ulRef.append(...markap);
+// console.log(ulRef);
+ 
