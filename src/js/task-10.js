38e80ec223de9c050a2fe86440ed01e24727ea51
YurkaBest <Ivaschenko_u@ukr.net>
2022-02-07 15:18:42 +0200

    add 10 task

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -2,11 +2,30 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 const refs = {
-  controls: document.querySelector("#controls"),
+  controls: document.getElementById("controls"),
   divList: document.querySelector("#boxes"),
   input: document.querySelector("input"),
+  btnCreate: document.querySelector("button[data-create]"),
+  btnDestroy: document.querySelector("button[data-destroy]"),
 };
 
-// refs.controls.addEventListener("submit", handleFormSubmit);
-// console.log(refs.controls);
-// console.log(refs.divList);
+let arr = [];
+let defAmount = 0;
+
+const handleCreate = () => {
+  const count = refs.input.value;
+
+  for (let i = 0; i < count; i += 1) {
+    const liRef = document.createElement("div");
+    liRef.style.width = 30 + 10 * (defAmount + i) + "px";
+    liRef.style.height = 30 + 10 * (defAmount + i) + "px";
+
+    liRef.style.backgroundColor = getRandomHexColor();
+
+    arr.push(liRef);
+  }
+  defAmount += +count;
+  refs.divList.append(...arr);
+};
+
+refs.btnCreate.addEventListener("click", handleCreate);
